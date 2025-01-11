587f2156e6d14679c16092521e9966e5c32d85a7
dbchristenson <dbchristenson@uchicago.edu>
2025-01-12 01:23:29 +0800

    first card implementation

--- ccsite/ccapp/models.py
+++ ccsite/ccapp/models.py
@@ -2,29 +2,51 @@ from django.db import models
 
 
 class Rarity(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=32)
 
     def __str__(self):
         return self.name
 
 
 class Move(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=32)
     description = models.TextField()
     damage = models.IntegerField()
 
+    energy_requirements = models.ManyToManyField(
+        "EnergyType", through="MoveEnergyRequirement"
+    )
+
 
 class CardType(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=16)
 
     def __str__(self):
         return self.name
 
 
+class EnergyType(models.Model):
+    name = models.CharField(max_length=16)
+    symbol = models.ImageField(
+        upload_to="energy_symbols/", blank=True, null=True
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class MoveEnergyRequirement(models.Model):
+    move = models.ForeignKey(Move, on_delete=models.CASCADE)
+    energy = models.ForeignKey(EnergyType, on_delete=models.CASCADE)
+    amount = models.IntegerField()
+
+
 class Card(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=16)
     description = models.TextField()
-    image = models.ImageField(upload_to="images/")
+    image = models.ImageField(
+        upload_to="card_illustrations/", blank=True, null=True
+    )
 
     rarity = models.ForeignKey(Rarity, on_delete=models.CASCADE)
     card_type = models.ForeignKey(CardType, on_delete=models.CASCADE)
@@ -35,9 +57,11 @@ class Card(models.Model):
 
 
 class Collection(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=64)
     description = models.TextField()
-    image = models.ImageField(upload_to="images/")
+    image = models.ImageField(
+        upload_to="collection_illustrations/", blank=True, null=True
+    )
 
     cards = models.ManyToManyField(Card)
 
