dafc37faea7a427a21bbf93dc62d8e46574e86a0
dbchristenson <dbchristenson@uchicago.edu>
2025-01-12 20:07:05 +0800

    added models to admin page

--- ccsite/ccapp/admin.py
+++ ccsite/ccapp/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import (
+    Ability,
+    Card,
+    CardType,
+    EnergyType,
+    Move,
+    MoveEnergyRequirement,
+    Rarity,
+)
+
+admin.site.register(Ability)
+admin.site.register(Card)
+admin.site.register(CardType)
+admin.site.register(EnergyType)
+admin.site.register(Move)
+admin.site.register(MoveEnergyRequirement)
+admin.site.register(Rarity)
